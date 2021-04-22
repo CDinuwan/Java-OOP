b36144de52479388ae2bedd62b5c4b336287d53f
CDinuwan <chanukadinuwan35@gmail.com>
2021-04-22 10:07:04 +0530

    JUnit testing

---
+++ jUnit installation/src/junitinstall/junit_install.java
@@ -0,0 +1,10 @@
+package junitinstall;
+
+import org.junit.jupiter.api.Test;
+
+public class junit_install {
+	@Test
+	public void myJunitInstall() {
+		System.out.println("Welcome to my JUnit Testing");
+	}
+}
